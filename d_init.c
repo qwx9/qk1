47b45ecfc357fa61be133e776d3d84f8f9d0b152
Sigrid Solveig Haflínudóttir <sigrid@ftrv.se>
2023-10-16 09:57:03 +0000

    d_init: remove unused stuff

--- d_init.c
+++ d_init.c
@@ -40,59 +40,6 @@ void D_Init (void)
 	r_aliasuvscale = 1.0;
 }
 
-
-/*
-===============
-D_CopyRects
-===============
-*/
-void D_CopyRects (vrect_t *prects, int transparent)
-{
-
-// this function is only required if the CPU doesn't have direct access to the
-// back buffer, and there's some driver interface function that the driver
-// doesn't support and requires Quake to do in software (such as drawing the
-// console); Quake will then draw into wherever the driver points vid.buffer
-// and will call this function before swapping buffers
-
-	USED(prects);
-	USED(transparent);
-}
-
-
-/*
-===============
-D_EnableBackBufferAccess
-===============
-*/
-void D_EnableBackBufferAccess (void)
-{
-	VID_LockBuffer ();
-}
-
-
-/*
-===============
-D_TurnZOn
-===============
-*/
-void D_TurnZOn (void)
-{
-// not needed for software version
-}
-
-
-/*
-===============
-D_DisableBackBufferAccess
-===============
-*/
-void D_DisableBackBufferAccess (void)
-{
-	VID_UnlockBuffer ();
-}
-
-
 /*
 ===============
 D_SetupFrame
