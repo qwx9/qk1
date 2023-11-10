73fca33b06e2ebb61473d295fdd39f8bbf57e4dd
Sigrid Solveig Haflínudóttir <sigrid@ftrv.se>
2023-11-10 20:32:59 +0100

    physics: don't touch the triggers when in noclip mode (thanks mh)

--- sv_phys.c
+++ sv_phys.c
@@ -890,7 +890,7 @@ void SV_Physics_Client (edict_t	*ent, int num)
 	}
 
 	// call standard player post-think
-	SV_LinkEdict (ent, true);
+	SV_LinkEdict (ent, (int)ent->v.movetype != MOVETYPE_NOCLIP);
 
 	pr_global_struct->time = sv.time;
 	pr_global_struct->self = EDICT_TO_PROG(ent);
