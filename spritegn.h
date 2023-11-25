41b4118b3aaed6b19c909377662ac71dc0aa7ce4
Sigrid Solveig Haflínudóttir <sigrid@ftrv.se>
2023-11-25 20:25:16 +0100

    remove unused on-disk structs for sprites loading

--- spritegn.h
+++ spritegn.h
@@ -15,36 +15,5 @@ typedef enum {
 	SPR_GROUP,
 }spriteframetype_t;
 
-// TODO: shorten these?
-typedef struct {
-	int			ident;
-	int			version;
-	int			type;
-	float		boundingradius;
-	int			width;
-	int			height;
-	int			numframes;
-	float		beamlength;
-	synctype_t	synctype;
-} dsprite_t;
-
-typedef struct {
-	int			origin[2];
-	int			width;
-	int			height;
-} dspriteframe_t;
-
-typedef struct {
-	int			numframes;
-} dspritegroup_t;
-
-typedef struct {
-	float	interval;
-} dspriteinterval_t;
-
-typedef struct {
-	spriteframetype_t	type;
-} dspriteframetype_t;
-
 #define IDSPRITEHEADER	(('P'<<24)+('S'<<16)+('D'<<8)+'I')
 // little-endian "IDSP"
