67e63b7e1b4b799192b3e9c8a9702e54c83b67d1
Sigrid Solveig Haflínudóttir <sigrid@ftrv.se>
2023-12-18 00:05:16 +0100

    blendalpha: leave dest color attributes

--- d_local.h
+++ d_local.h
@@ -82,10 +82,11 @@ blendalpha(pixel_t ca, pixel_t cb, int alpha)
 		a = (alpha*((ca>> 0)&0xff) + 255*((cb>> 0)&0xff))>> 8;
 		b = (alpha*((ca>> 8)&0xff) + 255*((cb>> 8)&0xff))>> 8;
 		c = (alpha*((ca>>16)&0xff) + 255*((cb>>16)&0xff))>> 8;
-		return min(a, 255) | min(b, 255)<<8 | min(c, 255)<<16;
+		return (cb & 0xff000000) | min(a, 255) | min(b, 255)<<8 | min(c, 255)<<16;
 	}
 
 	return
+		(cb & 0xff000000) |
 		((alpha*((ca>> 0)&0xff) + (255-alpha)*((cb>> 0)&0xff))>> 8) << 0 |
 		((alpha*((ca>> 8)&0xff) + (255-alpha)*((cb>> 8)&0xff))>> 8) << 8 |
 		((alpha*((ca>>16)&0xff) + (255-alpha)*((cb>>16)&0xff))>> 8) << 16;
